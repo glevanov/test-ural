5171a8d36a856775231d7033b63f9ff79af34b86
Grigoriy Levanov <glevanov@gmail.com>
2019-07-01 14:37:55 +0300

    Add Swiper template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Init')
+var mySwiper = new Swiper ('.swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
